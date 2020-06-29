5381af86b29e14b3818106ca88a230cb53bb9158
Shikhar0051 <48147323+Shikhar0051@users.noreply.github.com>
2020-06-29 21:47:11 +0530

    updated features

--- set.py
+++ set.py
@@ -1,6 +1,9 @@
 from flask import Flask,render_template,request
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 app = Flask(__name__)
 
 
@@ -16,13 +19,20 @@ headers_amaz = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
 }
 
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--profile-directory=Default') 
+driver = webdriver.Chrome(options=options)
+
+
 
 #to get flipkart product name for technologies
 def flip_prize(product,Flag):
     url='https://www.flipkart.com/search?q=' + product + '&otracker=AS_Query_HistoryAutoSuggest_2_0&otracker1=AS_Query_HistoryAutoSuggest_2_0&marketplace=FLIPKART&as-show=on&as=off&as-pos=2&as-type=HISTORY'
-    response = requests.get(url, headers=headers_flip)
-    soup= BeautifulSoup(response.text,'html.parser')
-    main_box= soup.find_all('div',{"class":"_3O0U0u"})
+    driver.get(url)
+    html = driver.page_source
+    page = BeautifulSoup(html)
+    main_box= page.find_all('div',{"class":"_3O0U0u"})
     box=main_box[0]
     temp=[]
     try:
@@ -33,11 +43,12 @@ def flip_prize(product,Flag):
                 l=s+link['href']
                 #print(l)
                 val=box.find("div",{"class":"_3wU53n"}).text.strip()
-                
+                product_img = box.find('img',{'class':'_1Nyybr'}).get('src')
                 #print(val)
                 if product.lower() in val.lower():
                     price=box.find("div",{"class":"_1vC4OE _2rQ-NK"}).text.strip()
-                    temp.append([l,val,price])
+                    temp.append([l,product_img,val,price])
+                    #print(temp)
         else:
             for box in main_box:
                 s="https://www.flipkart.com"
@@ -46,7 +57,9 @@ def flip_prize(product,Flag):
                 l=s+link['href']
                 title=box.find("div",{"class":"_3wU53n"}).text.strip()
                 price=box.find("div",{"class":"_1vC4OE _2rQ-NK"}).text.strip()
-                temp.append([l,title,price])
+                product_img = box.find('img',{'class':'_1Nyybr'}).get('src')
+                temp.append([l,product_img,title,price])
+                #print(temp)
     except:
         i=1
     #print(temp)
@@ -59,36 +72,42 @@ def amaz_price(product,Flag):
     soup= BeautifulSoup(response.text,'html.parser')
     main_box= soup.find_all('div',{"class":"s-include-content-margin s-border-bottom s-latency-cf-section"})
     temp=[]
-    try:
-        if Flag==False:
-            for box in main_box:
-                s="https://www.amazon.in"
-                link=box.find("a",{"class":"a-link-normal a-text-normal"},href=True)
-                l=s+link['href']
-                val=box.find("span",{"class":"a-size-medium a-color-base a-text-normal"}).text.strip()
-                
-                if product.lower() in val.lower():
-                    price=box.find("span",{"class":"a-price-whole"}).text.strip()
-                    temp.append([l,val,price])
-        else:
-            for box in main_box:
-                s="https://www.amazon.in"
-                link=box.find("a",{"class":"a-link-normal a-text-normal"},href=True)
-                l=s+link['href']
-                title=box.find("span",{"class":"a-size-medium a-color-base a-text-normal"}).text.strip()
-                price=box.find("span",{"class":"a-price-whole"}).text.strip()
-                temp.append([l,title,price])
-    except:
-        i=1
+    if Flag==False:
+        for box in main_box:
+            s="https://www.amazon.in"
+            link=box.find("a",{"class":"a-link-normal a-text-normal"},href=True)
+            l=s+link['href']
+            val=box.find("span",{"class":"a-size-medium a-color-base a-text-normal"}).text.strip()
+            product_img=box.find("img",{"class":"s-image"}).get('src')
+            if product.lower() in val.lower():
+                pr=box.find("span",{"class":"a-price-whole"})
+                price= pr.text.strip() if pr else "N/A"
+                if price!="N/A":
+                    temp.append([l,product_img,val,price])
+                    #print(temp)
+    else:
+        for box in main_box:
+            s="https://www.amazon.in"
+            link=box.find("a",{"class":"a-link-normal a-text-normal"},href=True)
+            l=s+link['href']
+            title=box.find("span",{"class":"a-size-medium a-color-base a-text-normal"}).text.strip()
+            pr=box.find("span",{"class":"a-price-whole"})
+            price= pr.text.strip() if pr else "N/A"
+            product_img=box.find("img",{"class":"s-image"}).get('src')
+            if price!="N/A":
+                temp.append([l,product_img,title,price])
+                #print(temp)
+    
     #print(temp)
     return temp
 
 #to get flipkart product name for others
 def flip_app_price(product):
     url='https://www.flipkart.com/search?q=' + product + '&otracker=AS_Query_HistoryAutoSuggest_2_0&otracker1=AS_Query_HistoryAutoSuggest_2_0&marketplace=FLIPKART&as-show=on&as=off&as-pos=2&as-type=HISTORY'
-    response = requests.get(url, headers=headers_flip)
-    soup= BeautifulSoup(response.text,'html.parser')
-    main_box= soup.find_all('div',{"class":"IIdQZO _1SSAGr"})
+    driver.get(url)
+    html = driver.page_source
+    page = BeautifulSoup(html)
+    main_box= page.find_all('div',{"class":"IIdQZO _1SSAGr"})
     box=main_box[0]
     temp=[]
     try:
@@ -98,7 +117,8 @@ def flip_app_price(product):
             l=s+link['href']
             title= box.find("a",{"class":"_2mylT6"}).text.strip()
             price= box.find("div",{"class":"_1vC4OE"}).text.strip()
-            temp.append([l,title,price])
+            product_img = box.find('img',{'class':'_3togXc'}).get('src')
+            temp.append([l,product_img,title,price])
     except:
         i=1
     return temp
@@ -120,7 +140,8 @@ def amaz_app_price(product):
             l=s+link['href']
             title=box.find("span",{"class":"a-size-base-plus a-color-base a-text-normal"}).text.strip()
             price=box.find("span",{"class":"a-offscreen"}).text.strip()
-            temp.append([l,title,price])
+            product_img=box.find("img",{"class":"s-image"}).get('src')
+            temp.append([l,product_img,title,price])
     except:
         i=1
     return temp
@@ -148,14 +169,9 @@ def getValue():
                 Flag = True
                 break
         
-        #print(Flag)
-        #getData(product_name)
         flip_list=flip_prize(product_name,Flag)
         amaz_list=amaz_price(product_name,Flag)
         
-        #if words[:] in list(product_name):
-         #   return render_template('show_product.html', p=product_name, li_amaz=amaz_list)
-        #else:
         return render_template('pass.html',p=product_name, li=flip_list, li_amaz=amaz_list)
     
     else:
